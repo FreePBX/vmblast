9a80aba3913612e88452521dca5bd763bcc7a891
Philippe Lindheimer <philippe.lindheimer@schmoozecom.com>
2010-05-31 20:08:26 +0000

    closes #4310 define or check for undefined vars as appropriate, music module create intermediate directories if not there

--- install.php
+++ install.php
@@ -6,37 +6,49 @@
 
 global $db;
 
-echo _("Upgrading vmblast to add audio_label field..");
+if (! function_exists("out")) {
+	function out($text) {
+		echo $text."<br />";
+	}
+}
+
+if (! function_exists("outn")) {
+	function outn($text) {
+		echo $text;
+	}
+}
+
+outn(_("Upgrading vmblast to add audio_label field.."));
 $sql = "SELECT audio_label FROM vmblast";
 $confs = $db->getRow($sql, DB_FETCHMODE_ASSOC);
 if (!DB::IsError($confs)) { // no error... Already done
-	echo _("Not Required")."<br />";
+	out(_("Not Required"));
 } else {
 	$sql = "ALTER TABLE vmblast ADD audio_label INT ( 11 ) NOT NULL DEFAULT -1";
 	$results = $db->query($sql);
 	if(DB::IsError($results)) {
-	        die($results->getMessage());
+	        die_freepbx($results->getMessage());
 	}
-	echo _("Done")."<br />";
+	out(_("Done"));
 }
 
-echo _("Upgrading vmblast to add password field..");
+outn(_("Upgrading vmblast to add password field.."));
 $sql = "SELECT password FROM vmblast";
 $confs = $db->getRow($sql, DB_FETCHMODE_ASSOC);
 if (!DB::IsError($confs)) { // no error... Already done
-	echo _("Not Required")."<br />";
+	out(_("Not Required"));
 } else {
 	$sql = "ALTER TABLE vmblast ADD password VARCHAR ( 20 ) NOT NULL";
 	$results = $db->query($sql);
 	if(DB::IsError($results)) {
-	        die($results->getMessage());
+	        die_freepbx($results->getMessage());
 	}
-	echo _("Done")."<br />";
+	out(_("Done"));
 }
 
 // Drop grplist field but first pull it's data and put in new table
 //
-echo _("Dropping grplist..");
+outn(_("Dropping grplist.."));
 $sql = 'SELECT grpnum, grplist FROM vmblast';
 $confs = $db->getAll($sql, DB_FETCHMODE_ASSOC);
 if (!DB::IsError($confs)) { 
@@ -50,20 +62,21 @@ if (!DB::IsError($confs)) {
 	$compiled = $db->prepare("INSERT INTO vmblast_groups (grpnum, ext) values (?,?)");
 	$result   = $db->executeMultiple($compiled, $list);
 	if(DB::IsError($result)) {
-		echo _("error populating vmblast_groups table")."<br />";	
+		out(_("error populating vmblast_groups table"));
 		return false;
 	} else {
-		echo _("populated new table<br />Dropping old grplist field");
+		out(_("populated new table"));
+		outn(_("Dropping old grplist field.."));
 		$sql = "ALTER TABLE `vmblast` DROP `grplist`";
 		$results = $db->query($sql);
 		if(DB::IsError($results)) {
-			echo _("failed to drop field")."<br />";
+			out(_("failed to drop field"));
 		} else {
-			echo _("OK")."<br />";
+			out(_("OK"));
 		}
 	}
 } else {
-	echo _("Not Needed")."<br />";
+	out(_("Not Needed"));
 }
 
 ?>
