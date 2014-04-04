139c3e127be739a149252fedd20e5f3fe22f7482
Kevin McCoy <kevin@qloog.com>
2014-04-04 16:54:30 +0900

    FREEPBX-1953 #QA #comment Fixed vmblast to use new string validation scheme

--- page.vmblast.php
+++ page.vmblast.php
@@ -252,8 +252,12 @@ function checkGRP(theForm) {
 	defaultEmptyOK = true;
 	if (!isInteger(theForm.password.value))
 		return warnInvalid(theForm.password, msgInvalidPassword);
-	
-	defaultEmptyOK = false;	
+
+	var sizeDisplayName = "<?php echo vmblast_get_field_size('description', 35); ?>";	
+	defaultEmptyOK = false;
+        if (!isCorrectLength(theForm.description.value, sizeDisplayName))
+                return warnInvalid(theForm.description, "<?php echo _('The Group Description provided is too long.'); ?>")
+
 	if (!isAlphanumeric(theForm.description.value))
 		return warnInvalid(theForm.description, msgInvalidDescription);
 	
