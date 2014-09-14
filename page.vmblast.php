7660f216207a14492c685125cc848d989ae96f9a
Andrew Nagy <andrew.nagy@the159.com>
2014-09-13 20:29:11 -0400

    Redo module_hook calls to use static obj

--- page.vmblast.php
+++ page.vmblast.php
@@ -210,7 +210,7 @@ if ($action == 'delGRP') {
 
 <?php
 			// implementation of module hook
-			// object was initialized in config.php
+			$module_hook == moduleHook::create();
 			echo $module_hook->hookHtml;
 ?>
 			<tr>
